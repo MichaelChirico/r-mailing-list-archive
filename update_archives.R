97c11ce63d44e984667a6f1b20633535f08dbaf7
Michael Chirico <michaelchirico4@gmail.com>
2022-12-15 22:45:34 -0800

    update script after a long time, including support for r-announce

--- update_archives.R
+++ update_archives.R
@@ -1,45 +1,62 @@
 #!/usr/local/bin/Rscript
-library(rvest)
+library(xml2)
 
 URL_BASE ='https://stat.ethz.ch/pipermail'
+# POSIXlt facilitates formatting as quarter
 today = as.POSIXlt(Sys.time())
 mailing_lists = list(
   c(name = 'r-devel', current = format(today, '%Y-%B.txt')),
   c(name = 'r-package-devel',
     current = with(today, sprintf('%dq%d.txt', year + 1900L, mon %/% 3L + 1L))),
-  c(name = 'r-help', current = format(today, '%Y-%B.txt'))
+  c(name = 'r-help', current = format(today, '%Y-%B.txt')),
+  c(name = 'r-announce', current = format(today, '%Y'))
 )
 for (ii in seq_along(mailing_lists)) {
   this_list = mailing_lists[[ii]]
-  URL = file.path(URL_BASE, this_list['name'], '')
+  outdir = this_list[['name']]
 
-  outdir = this_list['name']
+  URL = file.path(URL_BASE, ourdir)
   dir.create(outdir, recursive = TRUE, showWarnings = FALSE)
 
   # Always re-write current period
-  extant = setdiff(list.files(outdir), this_list['current'])
+  extant_gz = outdir |>
+    list.files() |>
+    setdiff(this_list[['current']]) |>
+    paste0('.gz')
 
-  zips = URL %>% read_html %>%
+  zips = URL |>
+    read_html() |>
     # linked under "Gzip'd Text NNN KB"
-    html_nodes(xpath = '//a[contains(text(), "Gzip")]') %>%
+    xml_find_all('//a[contains(text(), "Gzip")]') |>
+    xml_attr('href') |>
     # only download new archives
-    html_attr('href') %>% setdiff(sprintf('%s.gz', extant)) %>%
-    # sort should be unnecessary
-    sprintf('%s%s', URL, . ) %>% sort
+    setdiff(extant_gz) |>
+    file.path(URL, ..gzpath = _ ) |>
+    # this should be unnecessary
+    sort()
 
   if (length(zips)) {
-    message('Acquiring ', length(zips), ' archives: ',
-            basename(head(zips, 1L)), ' - ', basename(tail(zips, 1L)))
+    message(sprintf(
+      'Acquiring %d %s archives: %s - %s',
+      length(zips), outdir, basename(head(zips, 1L)), basename(tail(zips, 1L))
+    ))
   } else {
-    message('No new zips to acquire')
+    message('No new ', outdir, ' archives to acquire')
   }
 
   for (zip in zips) {
-    download.file(zip, tmp <- tempfile())
-    conn <- gzfile(tmp)
-    outfile <- gsub('.gz', '', basename(zip), fixed = TRUE)
-    writeLines(readLines(conn), file.path(outdir, outfile))
-    close(conn)
-    unlink(tmp)
+    local({
+      zip_tmp <- tempfile()
+      on.exit(unlink(zip_tmp))
+      download.file(zip, zip_tmp)
+      local({
+        zip_conn <- gzfile(zip_tmp)
+        on.exit(close(zip_conn))
+        outfile <- gsub('.gz', '', basename(zip), fixed = TRUE)
+        zip_conn |>
+          readLines() |>
+          writeLines(file.path(outdir, outfile))
+      })
+    })
   }
 }
