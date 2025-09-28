43c4648d90e549987cabe59097df6297490cc882
Ben Bolker <bbolker@gmail.com>
2025-09-28 17:25:09 -0400

    add r-sig-mixed-models

--- update_archives.R
+++ update_archives.R
@@ -13,6 +13,8 @@ mailing_lists = list(
   c(name = 'r-announce', current = format(today, '%Y')),
   c(name = 'r-sig-geo', current = format(today, '%Y-%B.txt')),
   c(name = 'r-sig-finance',
+    current = with(today, sprintf('%dq%d.txt', year + 1900L, mon %/% 3L + 1L))),
+  c(name = 'r-sig-mixed-models',
     current = with(today, sprintf('%dq%d.txt', year + 1900L, mon %/% 3L + 1L)))
 )
 for (ii in seq_along(mailing_lists)) {
